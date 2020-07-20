f47996a1d3ded21ef39f2c4e716191ba027d9b95
shrutic640 <shruti.chatz@gmail.com>
2020-07-20 14:40:18 +1000

    NEW: Add app functionailty

---
+++ web/public/app.js
@@ -0,0 +1,28 @@
+$('#navbar').load('navbar.html');
+
+const devices = JSON.parse(localStorage.getItem('devices')) || [];
+
+devices.forEach(function (device) {
+    $('#devices tbody').append(`
+    <tr>
+    <td>${device.user}</td>
+    <td>${device.name}</td>
+    </tr>`
+    );
+});
+
+
+$('#add-device').on('click', function() {
+    const user = $('#user').val();
+   6 / 8
+    const name = $('#name').val();
+    devices.push({ user: user, name: name });
+    localStorage.setItem('devices', JSON.stringify(devices));
+    location.href = '/';
+   });
+   
+   $('#send-command').on('click', function() {
+    const command = $('#command').val();
+    console.log(`command is: ${command}`);
+   });
+
