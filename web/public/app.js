5e7b5dd50a3b02173a25ea0c1fd27a5289096d0d
shrutic640 <shruti.chatz@gmail.com>
2020-07-24 20:40:59 +1000

    New: Add registration and login feature

--- web/public/app.js
+++ web/public/app.js
@@ -1,5 +1,5 @@
 $('#navbar').load('navbar.html');
-$('#footer').load("footer.html"); 
+$('#footer').load("footer.html");
 
 const devices = JSON.parse(localStorage.getItem('devices')) || [];
 
@@ -13,17 +13,60 @@ devices.forEach(function (device) {
 });
 
 
-$('#add-device').on('click', function() {
+$('#add-device').on('click', function () {
     const user = $('#user').val();
-   6 / 8
     const name = $('#name').val();
     devices.push({ user: user, name: name });
     localStorage.setItem('devices', JSON.stringify(devices));
     location.href = '/';
-   });
-   
-   $('#send-command').on('click', function() {
+});
+
+$('#send-command').on('click', function () {
     const command = $('#command').val();
     console.log(`command is: ${command}`);
-   });
+});
+
+
+$('#register').on('click', function() {
+    const username = $('#username').val();
+    const password = $('#password').val();
+    const confirmpassword = $('#confirmpassword').val();
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    const userexists = users.find(user => user.name === username);
+
+    if (userexists) {
+        $('#message').appendTo('<p class="alert alert-info alert-dismissible fade in">This Username has already been taken</p>');
+    }
+
+    else if (password !== confirmpassword) {
+        $('#message').append('<p class="alert alert-warning alert-dismissible fade in">The passwords do not match</p>');
+    }
+    else {
+        users.push({ name: username, password });
+        localStorage.setItem('users', JSON.stringify(users));
+        location.href = '/login';
+    }
+});
+
+
+$('#login').on('click', function () {
+    const username = $('#username').val();
+    const password = $('#password').val();
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    const usernameexists = users.find(user => user.name === username);
+    const passwordexists = users.find(user => user.password === password);
+    if (usernameexists && passwordexists === password) {
+        localStorage.setItem('isAuthenticated', true);
+        location.href = '/';
+    } else {
+        $('#message').append('<p class="alert alert-warning alert-dismissible fade in">Wrong username or password</p>');
+    }
+});
+
+const logout = () => {
+    localStorage.removeItem('isAuthenticated');
+    location.href = '/login';
+}
+
+
 
