97891c949fb5c7169ed6dc053f42c4cf61ae5e05
shrutic640 <shruti.chatz@gmail.com>
2020-07-24 20:44:41 +1000

    ENHANCE: Add registration and login location

--- web/server.js
+++ web/server.js
@@ -20,6 +20,15 @@ app.get('/send-command', (req, res) => {
 app.get('/about', (req, res) => {
     res.sendFile(`${base}/about-me.html`);
    });
+
+
+app.get('/registration', (req, res) => {
+    res.sendFile(`${base}/registration.html`);
+   });
+   
+app.get('/login', (req, res) => {
+    res.sendFile(`${base}/login.html`);
+   });
    
 app.get('*', (req, res) => {
     res.sendFile(`${base}/404.html`);
